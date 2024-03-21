7d5f05363e616aa63cda013e490d252172d59c70
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2024-03-21 14:43:19 +0200

    ref:remove usetate hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import '@/styles/globals.css'
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-} from '@clerk/nextjs'
+
+import { ClerkProvider } from '@clerk/nextjs'
 import type { AppProps } from 'next/app'
 import { StoreContext } from '../../store'
 import { Store } from '../../store/Store'
-import { useEffect, useState } from 'react'
-import Editor from './editor'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [store] = useState(new Store())
