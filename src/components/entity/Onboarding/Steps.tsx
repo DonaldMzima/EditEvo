2801e6f0b11e91416a22c862b44ab0e3cb723f7b
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2024-04-03 10:05:13 +0200

    feat: add joyride onboarding process

---
+++ src/components/entity/Onboarding/Steps.tsx
@@ -0,0 +1,50 @@
+// JoyrideSteps.js
+import React from 'react'
+
+const JoyrideSteps = () => {
+  return [
+    {
+      target: '.welcome',
+      content: `Welcome to EditEvo! 🎉 Let's get started by exploring our creativity.`,
+    },
+    {
+      target: '.menu',
+      content: 'Use the menu to perform various actions.',
+    },
+    {
+      target: '.resources',
+      content:
+        'Click on any of the menu button to add video,audio,image,text,animation,Effect,fielter on you the editor and also export you video.',
+    },
+    {
+      target: '#grid-canvas-container',
+      content: 'This is the canvas where you can create your designs.',
+    },
+    {
+      target: '.footer',
+      content: ` Check out Donald's portfolio to explore more of his work. Stay connected with me on GitHub for updates and contributions`,
+    },
+    {
+      target: '.elements',
+      content: 'Delete Elements if necessary.',
+    },
+    {
+      target: '.timeline',
+      content: 'Use this timeline fofr adjustments.',
+    },
+    // {
+    //   target: '.text-menu-option',
+    //   content: 'Use the menu to perform various actions.',
+    // },
+    {
+      target: '.profile',
+      content: 'Click on the button to manage your profile or sign out.',
+    },
+    {
+      target: '.animation-menu-option',
+      content: `Feel free to explore and engage with our platform. If you have any questions or feedback, don't hesitate to reach out. Happy editing!🎉🎉🎉🎉🎉🎉`,
+    },
+  ]
+}
+
+export default JoyrideSteps
